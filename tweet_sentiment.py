ef908c0e56942891a6110f208d9008f9a5141736
Mari Muraki <marimuraki@gmail.com>
2013-05-08 19:43:39 -0700

    Initial commit

---
+++ tweet_sentiment.py
@@ -0,0 +1,4 @@
+# Compute sentiment score of tweet as the sum of sentiment scores of terms in tweet
+# 		python tweet_sentiment.py <sentiment_file> <tweet_file>
+#  		<sentiment_file> = AFINN-111.txt
+# 		<tweet_file> = output.json

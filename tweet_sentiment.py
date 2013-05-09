f9021834c938ba90c8a521bea069e0f4c1e124b5
Mari Muraki <marimuraki@gmail.com>
2013-05-08 19:46:46 -0700

    add function to read tweets from JSON file

--- tweet_sentiment.py
+++ tweet_sentiment.py
@@ -2,3 +2,12 @@
 # 		python tweet_sentiment.py <sentiment_file> <tweet_file>
 #  		<sentiment_file> = AFINN-111.txt
 # 		<tweet_file> = output.json
+
+import json
+
+def read_tweets(input_tweet_file):
+	tweets = []
+	json_data=open(input_tweet_file, 'r')
+	for line in json_data:
+		tweets.append(json.loads(line))
+	return tweets

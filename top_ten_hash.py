f2754b1f6839c607c328cee501c37f1b329707f8
Mari Muraki <marimuraki@gmail.com>
2013-05-13 17:34:32 -0700

    update to identify #hash using json data structure, not text parsing

--- top_ten_hash.py
+++ top_ten_hash.py
@@ -4,7 +4,6 @@
 
 from sys import argv
 from collections import Counter
-import csv
 import json
 
 def read_tweets(input_tweet_file):
@@ -13,21 +12,15 @@ def read_tweets(input_tweet_file):
     return tweets
 
 def parse_tweet(tweets):
-    tweet_list = [tweet["text"].encode('utf-8') for tweet in tweets]
-    tweet_words_list = [tweet.split() for tweet in tweet_list]
-    return tweet_list, tweet_words_list  
-        
-def list_terms(tweet_words_list):
-    term_list = []
-    for tweet in tweet_words_list:
-        for word in tweet:
-            if word.startswith("#"):
-                term_list.append(word)
-    return term_list
+    hash_list = []
+    for tweet in tweets:
+        for item in tweet["entities"]["hashtags"]:
+            hash_list.append(item["text"])
+    return hash_list  
  
-def tally(term_list):
+def tally(hash_list):
     tally = Counter()
-    for term in term_list:
+    for term in hash_list:
         tally[term] += 1
     toptenhash = tally.most_common(10)
     for hash in toptenhash:
@@ -35,6 +28,5 @@ def tally(term_list):
     
 if __name__ == '__main__':
     tweetslist = read_tweets(argv[1])
-    tweet, tweetwords = parse_tweet(tweetslist)
-    termlist = list_terms(tweetwords)
-    tally(termlist)
+    hash_list = parse_tweet(tweetslist)
+    tally(hash_list)
